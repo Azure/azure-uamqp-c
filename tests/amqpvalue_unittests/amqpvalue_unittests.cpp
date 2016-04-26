5b2758d02e3b02839ba479de654d265a4bb36112
dcristoloveanu <dcristo@microsoft.com>
2016-04-26 14:05:08 -0700

    Update uAMQP INITIALIZE_MEMORY_DEBUG to be TEST_INITILIZE_MEMORY_DEBUG

--- tests/amqpvalue_unittests/amqpvalue_unittests.cpp
+++ tests/amqpvalue_unittests/amqpvalue_unittests.cpp
@@ -112,7 +112,7 @@ BEGIN_TEST_SUITE(amqpvalue_unittests)
 
         TEST_SUITE_INITIALIZE(setsBufferTempSize)
         {
-            INITIALIZE_MEMORY_DEBUG(g_dllByDll);
+            TEST_INITIALIZE_MEMORY_DEBUG(g_dllByDll);
             g_testByTest = MicroMockCreateMutex();
             ASSERT_IS_NOT_NULL(g_testByTest);
         }
@@ -120,7 +120,7 @@ BEGIN_TEST_SUITE(amqpvalue_unittests)
         TEST_SUITE_CLEANUP(TestClassCleanup)
         {
             MicroMockDestroyMutex(g_testByTest);
-            DEINITIALIZE_MEMORY_DEBUG(g_dllByDll);
+            TEST_DEINITIALIZE_MEMORY_DEBUG(g_dllByDll);
         }
 
         TEST_FUNCTION_INITIALIZE(f)

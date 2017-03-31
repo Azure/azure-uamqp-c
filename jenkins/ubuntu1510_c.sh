df563347e10278042658db071b6b358bd1735750
dcristo <dcristo@THINGIE42>
2017-03-30 19:25:21 -0700

    Update Ubuntu build script to run E2E tests

--- jenkins/ubuntu1510_c.sh
+++ jenkins/ubuntu1510_c.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
+#
 
-build_root=$(cd "$(dirname "$0")/.." && pwd)
-cd $build_root
+set -e
 
-# -- C --
-./build_all/linux/build.sh --run-unittests --run_valgrind "$@" #-x 
-[ $? -eq 0 ] || exit $?
+script_dir=$(cd "$(dirname "$0")" && pwd)
+build_root=$(cd "${script_dir}/.." && pwd)
+log_dir=$build_root
+make_install=
+build_folder=$build_root"/cmake"
 
+rm -r -f $build_folder
+mkdir -p $build_folder
+pushd $build_folder
+cmake .. -Drun_valgrind:BOOL=ON -Drun_unittests:bool=ON -Drun_e2e_tests:bool=ON
+cmake --build . -- --jobs=$(nproc)
+ctest -C "debug" -V
+if [[ $make_install == 1 ]] ;
+then
+    echo "Installing packaging" 
+    # install the package
+    make install
+fi
+
+popd
+:

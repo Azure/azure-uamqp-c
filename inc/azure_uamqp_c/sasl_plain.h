20c1512beac82baefb6b6ef105afeb1586dc35e2
dcristo <dcristo@THINGIE42>
2017-05-01 20:54:57 -0700

    Beautify specs for sasl_plain and nitpick update on tests

--- inc/azure_uamqp_c/sasl_plain.h
+++ inc/azure_uamqp_c/sasl_plain.h
@@ -19,11 +19,6 @@ extern "C" {
 		const char* authzid;
 	} SASL_PLAIN_CONFIG;
 
-	MOCKABLE_FUNCTION(, CONCRETE_SASL_MECHANISM_HANDLE, saslplain_create, void*, config);
-	MOCKABLE_FUNCTION(, void, saslplain_destroy, CONCRETE_SASL_MECHANISM_HANDLE, sasl_mechanism_concrete_handle);
-	MOCKABLE_FUNCTION(, int, saslplain_get_init_bytes, CONCRETE_SASL_MECHANISM_HANDLE, sasl_mechanism_concrete_handle, SASL_MECHANISM_BYTES*, init_bytes);
-	MOCKABLE_FUNCTION(, const char*, saslplain_get_mechanism_name, CONCRETE_SASL_MECHANISM_HANDLE, sasl_mechanism);
-	MOCKABLE_FUNCTION(, int, saslplain_challenge, CONCRETE_SASL_MECHANISM_HANDLE, concrete_sasl_mechanism, const SASL_MECHANISM_BYTES*, challenge_bytes, SASL_MECHANISM_BYTES*, response_bytes);
 	MOCKABLE_FUNCTION(, const SASL_MECHANISM_INTERFACE_DESCRIPTION*, saslplain_get_interface);
 
 #ifdef __cplusplus

05111def87c0ff54958ab7bf2e804fed8622be23
Andreas Wass <andreas.wass@dalelven.com>
2017-07-07 09:36:02 +0200

    Do not delete messages in on_link_state_changed on detach or error
    Messages will be deleted when pending deliveries are deleted instead.

--- src/message_sender.c
+++ src/message_sender.c
@@ -562,7 +562,6 @@ static void on_link_state_changed(void* context, LINK_STATE new_link_state, LINK
         {
             /* User initiated transition, we should be good */
             set_message_sender_state(message_sender_instance, MESSAGE_SENDER_STATE_IDLE);
-            indicate_all_messages_as_error(message_sender_instance);
         }
         else if (message_sender_instance->message_sender_state != MESSAGE_SENDER_STATE_IDLE)
         {
@@ -574,7 +573,6 @@ static void on_link_state_changed(void* context, LINK_STATE new_link_state, LINK
         if (message_sender_instance->message_sender_state != MESSAGE_SENDER_STATE_ERROR)
         {
             set_message_sender_state(message_sender_instance, MESSAGE_SENDER_STATE_ERROR);
-            indicate_all_messages_as_error(message_sender_instance);
         }
         break;
     }
